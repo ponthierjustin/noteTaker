acd2452a528008b280cb4f61b1291588f1904736
Justin <ponthierjustin@gmail.com>
2020-07-07 19:49:48 -0400

    added variable to create ids specific to note created

--- routes/api.js
+++ routes/api.js
@@ -6,17 +6,21 @@ module.exports = function (app) {
     res.json(notesData);
   });
 
-  app.post("/api/notes", function (req, res) {
-    notesData.push(req.body);
-    fs.writeFile(__dirname + "/../db/db.json", JSON.stringify(notesData), "utf-8", (err) => {
+  app.post("/api/notes", function (req, res) { 
+    let newNote = req.body;
+    newNote.id = notesData.length;
+    console.log(newNote);
+    notesData.push(newNote);
+    fs.writeFile(__dirname + "/../db/db.json", JSON.stringify(notesData, null, 4), "utf-8", (err) => {
       if (err) throw err;
       console.log("successful");
     });
     res.json(notesData);
   });
 
-  app.delete("/api/notes/title", function(req, res){
-    let chosen = req.params.notesData;
+  app.delete("/api/notes/:id", function(req, res){
+    let chosen = req.params.id;
+    res.json(chosen);
   });
 
 };
