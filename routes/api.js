54659cc4f5d7ad641be15e65374883a01f91764b
Justin <ponthierjustin@gmail.com>
2020-07-07 21:12:34 -0400

    notes can now be deleted

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,7 @@ module.exports = function (app) {
 
   app.post("/api/notes", function (req, res) { 
     let newNote = req.body;
-    newNote.id = notesData.length;
+    newNote.id = notesData.length + 1;
     console.log(newNote);
     notesData.push(newNote);
     fs.writeFile(__dirname + "/../db/db.json", JSON.stringify(notesData, null, 4), "utf-8", (err) => {
@@ -18,8 +18,15 @@ module.exports = function (app) {
     res.json(notesData);
   });
 
+  /* https://www.w3schools.com/jsref/jsref_filter.asp */
+  /* data = data.filter(x=>x.id!=req.params.id) */
   app.delete("/api/notes/:id", function(req, res){
     let chosen = req.params.id;
+    notesData = notesData.filter(note => note.id != chosen);
+    fs.writeFile(__dirname + "/../db/db.json", JSON.stringify(notesData, null, 4), "utf-8", (err) => {
+      if (err) throw err;
+      console.log("successful");
+    });
     res.json(chosen);
   });
 
